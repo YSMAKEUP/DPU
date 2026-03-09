ec6c00341a9872aee620efb018f7b83547d30fe2
Yunki_Song <rldbs111466@gmail.com>
2026-03-09 16:50:47 +0900

    UserService - @Transactional 추가 user.setPassword(passwordEncoder.encode(dto.getNewPassword())); 로직 수정
    UserViewController - System.out.println 비밀번호 , 이메일 ,권한 등 삭제

--- src/main/java/com/dpu/User/service/UserService.java
+++ src/main/java/com/dpu/User/service/UserService.java
@@ -3,9 +3,12 @@ package com.dpu.User.service;
 import com.dpu.User.domain.User;
 import com.dpu.User.dto.*;
 import com.dpu.User.repository.UserRepository;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -17,6 +20,7 @@ public class UserService {
 
 
     //회원가입
+    @Transactional
     public SignUpResponseDto signUp(SignUpRequestDto requestDto) {
 
         //검증 먼저 필요 왜? 불필요한 객체 생성을 막기 위해서
@@ -45,6 +49,7 @@ public class UserService {
     }
 
     //로그인
+    @Transactional
     public LoginResponseDto login(LoginRequestDto requestDto) {
         User user = userRepository.findByEmail(requestDto.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않은 이메일입니다."));
@@ -62,11 +67,13 @@ public class UserService {
                 .build();
     }
     //단건조회
+    @Transactional(readOnly = true)
     public User findById(Long userId){
         return userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
     }
 
+    @Transactional
     public void updateUser(Long id, UserUpdateDto dto) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("회원 없음"));
@@ -78,8 +85,10 @@ public class UserService {
             if (!dto.getNewPassword().equals(dto.getConfirmPassword())) {
                 throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
             }
-            user.setPassword(dto.getNewPassword());
+            user.setPassword(passwordEncoder.encode(dto.getNewPassword()));
         }
+        userRepository.save(user);
+
     }
 
 
